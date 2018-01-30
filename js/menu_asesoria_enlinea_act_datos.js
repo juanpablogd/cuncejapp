27cd072b084da56e8e2d06cdfab444f67fe4bfd9
Juan Pablo GD <juanpablogarzond@gmail.com>
2018-01-30 16:41:51 -0500

    asesoria en línea parte Uno

--- js/menu_asesoria_enlinea_act_datos.js
+++ js/menu_asesoria_enlinea_act_datos.js
@@ -3,7 +3,7 @@ window.onload = function() {
     var obj = JSON.parse(localStorage.getItem("usuario")); //console.log(validator.showErrors(obj));
     $.each(obj, function( index, value ) {      console.log(value + " " + index);
         if(index == "cedula"){
-            $('#myIframe').attr('src', 'http://www5.cundinamarca.gov.co/cuncejapp/menu_asesoria_enlinea.php?busca=' + value);
+            $('#myIframe').attr('src', 'http://www5.cundinamarca.gov.co/cuncejapp/menu_asesoria_enlinea_act_datos.php?busca=' + value);
         }
     });
 };
